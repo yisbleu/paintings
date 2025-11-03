fb447c1833ca53e27c0734974fe5ca2b6a4c090a
yisbleu <carribeanpalm72@gmail.com>
2025-11-02 20:45:43 -0500

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById("theme-toggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
